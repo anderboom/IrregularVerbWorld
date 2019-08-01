675fdf395e78fbcfbfd5ae82841f747968175141
Andrew <>
2019-08-01 20:45:43 +0300

    alpha 0.35

--- irregularVerbs/Extensions/UIButton+Extension.swift
+++ irregularVerbs/Extensions/UIButton+Extension.swift
@@ -11,7 +11,7 @@ import UIKit
 extension UIButton {
     open override var isEnabled: Bool {
         didSet {
-            alpha = isEnabled ? 1.0 : 0.5
+            alpha = isEnabled ? 1.0 : 0.35
         }
     }
 }
